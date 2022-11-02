966f47b08aaa4053023667b9b668b99d14543531
Victor Sonck <victor.sonck@gmail.com>
2022-11-02 16:07:54 +0100

    Completed check_clearml_task_running

--- check_clearml_task_running.py
+++ check_clearml_task_running.py
@@ -1,22 +1,41 @@
 import os
+import sys
+import time
 from clearml import Task
 
 
-def check_task_status(task_id):
+def check_task_status(task_id, timeout=600):
+    # Get the task object
     task = Task.get_task(task_id=task_id)
+    start_time = time.time()
     if task:
-        task_status = task.get_status()
-        # Try to get the task stats
-        if task_status in ["completed", "published", "publishing", "in_progress", "stopped", "failed"]:
-            pass
-        elif task_status in ["created", "queued", "unknown"]:
-            return f"Task is in {task_status} status, no stats yet."
+        while time.time() - start_time < timeout:
+            task_status = task.get_status()
+            print(task_status)
+            print(task.get_last_iteration())
+
+            if task_status == 'queued':
+                # If queued, just reset the timeout timer
+                start_time = time.time()
+            if task_status in ['failed', 'stopped']:
+                raise ValueError("Task did not run correctly, check logs in webUI.")
+            elif task_status == 'in_progress':
+                # Try to get the first iteration metric
+                if task.get_last_iteration() > 0:
+                    task.mark_stopped()
+                    task.set_archived(True)
+                    return True
+            time.sleep(5)
+        raise ValueError('Triggered Timeout!')
     else:
         return f"Can not find task {task}.\n\n"
-    
+
 
 if __name__ == '__main__':
     # get credentials and login info
-    os.environ["CLEARML_API_ACCESS_KEY"] = os.getenv('INPUT_CLEARML_API_ACCESS_KEY')
-    os.environ["CLEARML_API_SECRET_KEY"] = os.getenv('INPUT_CLEARML_API_SECRET_KEY')
-    os.environ["CLEARML_API_HOST"] = os.getenv('INPUT_CLEARML_API_HOST')
+    os.environ["CLEARML_API_ACCESS_KEY"] = os.getenv('CLEARML_API_ACCESS_KEY')
+    os.environ["CLEARML_API_SECRET_KEY"] = os.getenv('CLEARML_API_SECRET_KEY')
+    os.environ["CLEARML_API_HOST"] = os.getenv('CLEARML_API_HOST')
+    task_id = sys.argv[1]
+
+    check_task_status(task_id)
