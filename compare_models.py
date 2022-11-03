19fe6d83ec07a1ce13c487ae8407835dd28d10b5
Victor Sonck <victor.sonck@gmail.com>
2022-11-03 10:55:38 +0100

    add model compare

---
+++ compare_models.py
@@ -0,0 +1,25 @@
+import os
+from clearml import Task
+
+from tag_as_master import get_clearml_task_of_current_commit
+
+
+def compare_and_tag_task(commit_hash):
+    current_task = Task.get_task(task_id=get_clearml_task_of_current_commit(commit_hash))
+    best_task = Task.get_task(project_name='Github CICD', task_name='cicd_test', tags=['Best Performance'])
+    if best_task:
+        best_metric = max(
+            best_task.get_reported_scalars().get('Performance Metric').get('Series 1').get('y')
+        )
+        current_metric = max(
+            current_task.get_reported_scalars().get('Performance Metric').get('Series 1').get('y')
+        )
+        if current_metric >= best_metric:
+            current_task.add_tags(['Best Performance'])
+            # best_task.remove_tags(['Best Performance'])
+    else:
+        current_task.add_tags(['Best Performance'])
+
+
+if __name__ == '__main__':
+    compare_and_tag_task(os.getenv('GITHUB_SHA'))
--- task.py
+++ task.py
@@ -5,13 +5,13 @@ from tqdm import tqdm
 
 
 task = Task.init(
-    project_name='Github CI/CD',
+    project_name='Github CICD',
     task_name='dummy_task'
 )
 
 for i in tqdm(range(10)):
     task.get_logger().report_scalar(
-        title="Iteration Reporting",
+        title="Performance Metric",
         series="Series 1",
         iteration=i,
         value=random.randint(0, 100)
